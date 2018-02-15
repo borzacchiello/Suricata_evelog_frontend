f2cebfeb525b1dec3ce213e6925a0157c37f9d45
Luca Borzacchiello <lucaborza@gmail.com>
2018-02-15 17:53:46 +0100

    fixes

--- build_html.py
+++ build_html.py
@@ -158,7 +158,6 @@ def build_html(rows, page=0, order="timestamp_d", filter=default_filter):
     return compute_head(order, filter) + body + page_buttons(page, order, filter) + html_tail
 
 def build_table_packet(payload, packet):
-    return "<table border=\"1\"><tr><td class=\"column-message\"><b>payload<b></td></tr><tr><td>" + \
-                (payload if payload else "_empty_")+"</td></tr><tr><td class=\"column-message\">" + \
-                "<b>packet<b></td></tr><tr><td>"                     + \
-                (packet if packet else "_empty_")+"</td></tr></table>"
+    return "<table border=\"1\"><tr><td class=\"column-message\"><b>payload<b></td></tr><tr><td class=\"column-message\">" + \
+                (payload if payload else "_empty_")+"</td></tr><tr><td class=\"column-message\">"                          + \
+                "<b>packet<b></td></tr><tr><td  class=\"column-message\">" + (packet if packet else "_empty_")+"</td></tr></table>"
